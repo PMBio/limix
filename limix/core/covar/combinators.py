effa1b4e6dce9ac0cdefe95ca7b33ced7c07ae10
damien.arnol <damien.arnol@gmail.com>
2017-04-03 14:23:39 +0100

    implemented element wise product of any number of covariance, not tested yet

--- limix/core/covar/combinators.py
+++ limix/core/covar/combinators.py
@@ -108,3 +108,123 @@ class SumCov(ACombinatorCov):
                 return self.getCovariance(j).K_grad_interParam_i(idx)
             istart = istop
         return None
+
+class ProdCov(ACombinatorCov):
+    """
+    Element wise product of multiple covariance matrices.
+    The number of paramteters is the sum of the parameters of the single covariances.
+    """
+
+    def __init__(self,*covars):
+        """
+        Args:
+            covars:     covariances to be considered in the sum
+        """
+        ACombinatorCov.__init__(self)
+        self.dim = None
+        for covar in covars:
+            self.addCovariance(covar)
+
+    #####################
+    # Cached
+    #####################
+    # TODO test
+    @cached('covar_base')
+    def K(self):
+        K = sp.ones((self.dim,self.dim))
+        for i in range(len(self.covars)):
+            K *= self.getCovariance(i).K()
+        return K
+
+    # TODO test
+    @cached('covar_base')
+    def Kcross(self):
+        R = None
+        for i in range(len(self.covars)):
+            if not self.getCovariance(i).use_to_predict:    continue
+            if R is None:
+                R = self.covars[i].Kcross()
+            else:
+                _ = self.covars[i].Kcross()
+                assert _.shape[0]==R.shape[0], 'Dimension mismatch.'
+                assert _.shape[1]==R.shape[1], 'Dimension mismatch.'
+                R *= _
+        return R
+
+    @cached('covar_base')
+    # TODO test
+    def K_grad_i(self,i):
+        istart = 0
+        for j in range(len(self.covars)):
+            istop = istart + self.getCovariance(j).getNumberParams()
+            if (i < istop):
+                idx = i - istart
+                R = self.getCovariance(j).K_grad_i(idx)
+                for k in range(len(self.covars)):
+                    if k == j: continue
+                    R *= self.getCovariance(k).K()
+                return R
+            istart = istop
+        return None
+
+    # gradients from penalties on any term of the covariance
+    @cached('covar_base')
+    # TODO test
+    def penalty_grad(self, i):
+        istart = 0
+        for j in range(len(self.covars)):
+            istop = istart + self.getCovariance(j).getNumberParams()
+            if (i < istop):
+                idx = i - istart
+                return self.getCovariance(j).penalty_grad(idx)
+            istart = istop
+        return None
+
+    @cached('covar_base')
+    # TODO test
+    def K_hess_i_j(self, i, j):
+        istart = 0
+        jstart = 0
+        for c1 in range(len(self.covars)):
+            istop = istart + self.getCovariance(c1).getNumberParams()
+            if (i < istop):
+                i0 = i - istart
+                break
+            istart = istop
+        for c2 in range(len(self.covars)):
+            jstop = jstart + self.getCovariance(c2).getNumberParams()
+            if (j < jstop):
+                j0 = j - jstart
+                break
+            jstart = jstop
+        if c1==c2:
+            r = self.getCovariance(c1).K_hess_i_j(i0, j0)
+            for c3 in range(len(self.covars)):
+                if c3 == c1: continue
+                r *= self.getCovariance(c3).K()
+        else:
+            r = self.getCovariance(c1).K_grad_i(i0) * self.getCovariance(c2).K_grad_i(j0)
+            for c3 in range(len(self.covars)):
+                if c3 == c1: continue
+                if c3 == c2: continue
+                r *= self.getCovariance(c3).K()
+        return r
+
+    def _calcNumberParams(self):
+        self.n_params = 0
+        for i in range(len(self.covars)):
+            self.n_params += self.getCovariance(i).getNumberParams()
+        return self.n_params
+
+    ####################
+    # Interpretable Params
+    ####################
+    def K_grad_interParam_i(self,i):
+        istart = 0
+        for j in range(len(self.covars)):
+            istop = istart + self.getCovariance(j).getNumberParams()
+            if (i < istop):
+                idx = i - istart
+                return self.getCovariance(j).K_grad_interParam_i(idx)
+            istart = istop
+        return None
