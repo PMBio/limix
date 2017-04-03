67ccebf6d918cf2447ccefa159787852de3934f9
damien.arnol <damien.arnol@gmail.com>
2017-04-03 19:17:00 +0100

    modified test file

--- limix/core/covar/test/test_categorical.py
+++ limix/core/covar/test/test_categorical.py
@@ -5,10 +5,10 @@ import numpy as np
 from limix.core.covar import Categorical
 from limix.utils.check_grad import mcheck_grad
 
-class TestCategorical(unittest.TestCase):
+class TestCategoricalLowRank(unittest.TestCase):
     """test class for Categorical cov"""
     def setUp(self):
-        # sp.random.seed(1)
+        sp.random.seed(1)
         self.n = 30
         categories = sp.random.choice(['a', 'b', 'c'], self.n)
 
@@ -43,6 +43,33 @@ class TestCategorical(unittest.TestCase):
     #
     #     with self.assertRaises(ValueError):
     #         self.C.K_grad_i(0)
+
+class TestCategoricalFreeForm(unittest.TestCase):
+    """test class for Categorical cov"""
+    def setUp(self):
+        sp.random.seed(1)
+        self.n = 30
+        categories = sp.random.choice(['a', 'b', 'c'], self.n)
+
+        self.rank =None
+
+        self.C = Categorical(categories,self.rank)
+        self.name = 'categorical'
+        self.C.setRandomParams()
+
+    def test_grad(self):
+        def func(x, i):
+            self.C.setParams(x)
+            return self.C.K()
+
+        def grad(x, i):
+            self.C.setParams(x)
+            return self.C.K_grad_i(i)
+
+        x0 = self.C.getParams()
+        err = mcheck_grad(func, grad, x0)
+
+        np.testing.assert_almost_equal(err, 0., decimal = 6)
 
 if __name__ == '__main__':
     unittest.main()
